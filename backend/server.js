5289ca1d6a75ad8082d82a880dd3e13eb717e34e
shubham kumar <shubhofficial1@gmail.com>
2024-04-15 20:41:44 +0530

    Initial Setup for Backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running..");
+});
+
+const PORT = process.env.PORT || 5001;
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on port ${PORT} in ${NODE_ENV} mode`.yellow.bold
+  );
+});
