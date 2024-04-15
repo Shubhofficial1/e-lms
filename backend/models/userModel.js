3e1507b99ea4bf0f269485ca9ba79271ff64ce74
shubham kumar <shubhofficial1@gmail.com>
2024-04-16 01:16:38 +0530

    Added User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const emailRegexPatter = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: ["true", "Please enter your name"],
+    },
+    email: {
+      type: "string",
+      unique: true,
+      required: ["true", "Please enter your email"],
+      validate: {
+        validator: function (value) {
+          return emailRegexPatter.test(value);
+        },
+        message: "Please enter a valid email",
+      },
+    },
+    password: {
+      type: "string",
+      required: ["true", "Please enter your password"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+    avatar: {
+      public_id: string,
+      url: string,
+    },
+    role: {
+      type: "string",
+      default: "User",
+      enum: ["User", "Admin", "Instructor"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
