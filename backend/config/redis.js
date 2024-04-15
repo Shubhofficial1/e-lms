f998a1d5453dd7372a8547c462c854878ca395ee
shubham kumar <shubhofficial1@gmail.com>
2024-04-15 22:30:36 +0530

    Added Redis Config file

---
+++ backend/config/redis.js
@@ -0,0 +1,14 @@
+import { Redis } from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const redisClient = () => {
+  if (process.env.REDIS_URL) {
+    console.log("Redis connected");
+    return process.env.REDIS_URL;
+  } else {
+    throw new Error("Redis Connection failed.");
+  }
+};
+
+export const redis = new Redis(redisClient());
